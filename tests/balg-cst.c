9cd18c4743f84020eaf47206d27b55e0936bb67f
Metehan Selvi <me@metehanselvi.com>
2025-11-30 01:59:21 +0300

    tests(rdesc): increase test coverage: behavior in nomatch

---
+++ tests/balg-cst.c
@@ -0,0 +1,61 @@
+#include "../include/rdesc.h"
+#include "../include/cfg.h"
+#include "../include/util.h"
+#include "../src/detail.h" // IWYU pragma: keep
+#include "../src/exblex.h"
+
+#include "../examples/grammar/boolean-algebra.h"
+
+#include <stddef.h>
+
+
+int main()
+{
+	struct exblex lex;
+	struct rdesc_cfg cfg;
+	struct rdesc p;
+
+	struct rdesc_cfg_token tk;
+	struct rdesc_node *cst = NULL;
+
+	enum rdesc_result pump_res;
+
+	rdesc_cfg_init(&cfg, BALG_NT_COUNT, BALG_NT_VARIANT_COUNT,
+		       BALG_NT_BODY_LENGTH, (struct rdesc_cfg_symbol *) balg);
+	rdesc_init(&p, &cfg);
+
+	rdesc_start(&p, NT_STMT);
+	exblex_init(&lex,
+		    "{ a, b = (c = 0) | 1 & !test(1, 0), _123(); X_Y(); {;;} }",
+		    balg_tks, BALG_TK_COUNT);
+	while ((tk = exblex_next(&lex)).id != 0)
+		pump_res = rdesc_pump(&p, &cst, &tk);
+
+	assert(pump_res == RDESC_READY, "READY result expected");
+	assert(cst, "syntax tree could not be parsed");
+
+	rdesc_dump_dot(cst, balg_tk_printer_with_free, balg_nt_names, stdout);
+
+	exblex_init(&lex,
+		    "a =;",
+		    balg_tks, BALG_TK_COUNT);
+	rdesc_start(&p, NT_STMT);
+	while ((tk = exblex_next(&lex)).id != 0)
+		pump_res = rdesc_pump(&p, &cst, &tk);
+
+	assert(pump_res == RDESC_NOMATCH, "NOMATCH result expected");
+
+	struct rdesc_cfg_token *out;
+	size_t out_len;
+
+	rdesc_clearstack(&p, &out, &out_len);
+
+	for (size_t i = 0; i < out_len; i++)
+		if (out[i].id == TK_IDENT)
+			free(out[i].seminfo);
+	free(out);
+
+	rdesc_destroy(&p);
+	rdesc_cfg_destroy(&cfg);
+	rdesc_node_destroy(cst);
+}
