a6b71110500ad26cc4231477e567c9bed06faec0
metwse <me@metehanselvi.com>
2026-02-07 23:43:57 +0000

    Deploying to gh-pages from @ metwse/rdesc@8526a9cd08a96d1845a8697cc127b928f7bd4a85 🚀

--- search/all_2.js
+++ search/all_2.js
@@ -1,18 +1,17 @@
 var searchData=
 [
-  ['balg_0',['balg',['../boolean__algebra_8h.html#a48c8928f7b5b6de26ea3905042381f84',1,'boolean_algebra.h']]],
-  ['balg_5fnt_1',['balg_nt',['../boolean__algebra_8h.html#a824819f5190a8b390da1a30229ec73e9',1,'boolean_algebra.h']]],
-  ['balg_5fnt_5fbody_5flength_2',['BALG_NT_BODY_LENGTH',['../boolean__algebra_8h.html#a0eaae58589ac62e6b6e37e59a1be405f',1,'boolean_algebra.h']]],
-  ['balg_5fnt_5fcount_3',['BALG_NT_COUNT',['../boolean__algebra_8h.html#ae22dc60420bad18229307a7d3f5ecf46',1,'boolean_algebra.h']]],
-  ['balg_5fnt_5fnames_4',['balg_nt_names',['../boolean__algebra_8h.html#af69b0ef00608b4b7e99ad48335650110',1,'boolean_algebra.h']]],
-  ['balg_5fnt_5fvariant_5fcount_5',['BALG_NT_VARIANT_COUNT',['../boolean__algebra_8h.html#a59b5b3ecee08187bc53a3be8faa2911a',1,'boolean_algebra.h']]],
-  ['balg_5ftk_6',['balg_tk',['../boolean__algebra_8h.html#aa10441235dd946330f84b6e5dd4b5c42',1,'boolean_algebra.h']]],
-  ['balg_5ftk_5fcount_7',['BALG_TK_COUNT',['../boolean__algebra_8h.html#a7cbf9a1edb791896af995c324c0bfec3',1,'boolean_algebra.h']]],
-  ['balg_5ftk_5fnames_8',['balg_tk_names',['../boolean__algebra_8h.html#aca433d4ae632053f010e4fd1d58eb257',1,'boolean_algebra.h']]],
-  ['balg_5ftk_5fnames_5fescaped_9',['balg_tk_names_escaped',['../boolean__algebra_8h.html#a86e2dca21ab0a47b5596cd4b7f90e8e7',1,'boolean_algebra.h']]],
-  ['balg_5ftks_10',['balg_tks',['../boolean__algebra_8h.html#a52abe67416edb5d25d2511bcabc231bb',1,'boolean_algebra.h']]],
-  ['bc_2eh_11',['bc.h',['../bc_8h.html',1,'']]],
-  ['bnf_5fdsl_2eh_12',['bnf_dsl.h',['../bnf__dsl_8h.html',1,'']]],
-  ['boolean_5falgebra_2eh_13',['boolean_algebra.h',['../boolean__algebra_8h.html',1,'']]],
-  ['buf_14',['buf',['../structexblex.html#a4bfa1b98dcef53afeefe4d6983bee062',1,'exblex']]]
+  ['balg_5fnt_0',['balg_nt',['../boolean__algebra_8h.html#a824819f5190a8b390da1a30229ec73e9',1,'boolean_algebra.h']]],
+  ['balg_5fnt_5fbody_5flength_1',['BALG_NT_BODY_LENGTH',['../boolean__algebra_8h.html#a0eaae58589ac62e6b6e37e59a1be405f',1,'boolean_algebra.h']]],
+  ['balg_5fnt_5fcount_2',['BALG_NT_COUNT',['../boolean__algebra_8h.html#ae22dc60420bad18229307a7d3f5ecf46',1,'boolean_algebra.h']]],
+  ['balg_5fnt_5fnames_3',['balg_nt_names',['../boolean__algebra_8h.html#af69b0ef00608b4b7e99ad48335650110',1,'boolean_algebra.h']]],
+  ['balg_5fnt_5fvariant_5fcount_4',['BALG_NT_VARIANT_COUNT',['../boolean__algebra_8h.html#a59b5b3ecee08187bc53a3be8faa2911a',1,'boolean_algebra.h']]],
+  ['balg_5ftk_5',['balg_tk',['../boolean__algebra_8h.html#aa10441235dd946330f84b6e5dd4b5c42',1,'boolean_algebra.h']]],
+  ['balg_5ftk_5fcount_6',['BALG_TK_COUNT',['../boolean__algebra_8h.html#a7cbf9a1edb791896af995c324c0bfec3',1,'boolean_algebra.h']]],
+  ['balg_5ftk_5fnames_7',['balg_tk_names',['../boolean__algebra_8h.html#aca433d4ae632053f010e4fd1d58eb257',1,'boolean_algebra.h']]],
+  ['balg_5ftk_5fnames_5fescaped_8',['balg_tk_names_escaped',['../boolean__algebra_8h.html#a86e2dca21ab0a47b5596cd4b7f90e8e7',1,'boolean_algebra.h']]],
+  ['balg_5ftks_9',['balg_tks',['../boolean__algebra_8h.html#a52abe67416edb5d25d2511bcabc231bb',1,'boolean_algebra.h']]],
+  ['bc_2eh_10',['bc.h',['../bc_8h.html',1,'']]],
+  ['bnf_5fdsl_2eh_11',['bnf_dsl.h',['../bnf__dsl_8h.html',1,'']]],
+  ['boolean_5falgebra_2eh_12',['boolean_algebra.h',['../boolean__algebra_8h.html',1,'']]],
+  ['buf_13',['buf',['../structexblex.html#a4bfa1b98dcef53afeefe4d6983bee062',1,'exblex']]]
 ];
