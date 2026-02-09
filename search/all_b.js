b0dae96fe47a74f279bf756a8485bdffb2384155
metwse <me@metehanselvi.com>
2026-02-09 13:52:02 +0000

    Deploying to gh-pages from @ metwse/rdesc@5179c460ccf6f374c0bc7876e52c080fa165ec7a 🚀

--- search/all_b.js
+++ search/all_b.js
@@ -2,7 +2,8 @@ var searchData=
 [
   ['tk_0',['tk',['../structrdesc__node.html#ae713858dca30d7af39126e65a442a462',1,'rdesc_node']]],
   ['tk_1',['TK',['../bnf__dsl_8h.html#aa6abff88a7191ec249826ecc154edeae',1,'bnf_dsl.h']]],
-  ['token_5fcount_2',['token_count',['../structexblex.html#ac9c32459c8daa29aba3a15c7d1568a1b',1,'exblex']]],
-  ['tokens_3',['tokens',['../structexblex.html#a0178e84dd86b49a85dda68978c4d8b2c',1,'exblex::tokens()'],['../structrdesc__stack.html#ab0b40146845ef25dfe8c3d02030a66f7',1,'rdesc_stack::tokens()']]],
-  ['ty_4',['ty',['../structrdesc__cfg__symbol.html#a1bd12df565a2d599561d7b7ecf99cfe2',1,'rdesc_cfg_symbol::ty()'],['../structrdesc__node.html#adbac020e84364daa76e635992d801b2e',1,'rdesc_node::ty()']]]
+  ['tk_5fsize_2',['tk_size',['../structrdesc__stack.html#a2c85d77e42f80dcef2b48a5020bd17f3',1,'rdesc_stack']]],
+  ['token_5fcount_3',['token_count',['../structexblex.html#ac9c32459c8daa29aba3a15c7d1568a1b',1,'exblex']]],
+  ['tokens_4',['tokens',['../structexblex.html#a0178e84dd86b49a85dda68978c4d8b2c',1,'exblex::tokens()'],['../structrdesc__stack.html#a9575afc6b6209269a549a2c3936dea5f',1,'rdesc_stack::tokens()']]],
+  ['ty_5',['ty',['../structrdesc__cfg__symbol.html#a1bd12df565a2d599561d7b7ecf99cfe2',1,'rdesc_cfg_symbol::ty()'],['../structrdesc__node.html#adbac020e84364daa76e635992d801b2e',1,'rdesc_node::ty()']]]
 ];
--- search/typedefs_0.js
+++ search/typedefs_0.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['rdesc_5ftk_5fdestroyer_5ffunc_0',['rdesc_tk_destroyer_func',['../rdesc_8h.html#ad3f1f5fe8efcea1c72656b5c131fa67b',1,'rdesc.h']]],
-  ['rdesc_5ftk_5fprinter_5ffunc_1',['rdesc_tk_printer_func',['../util_8h.html#a5ad7949e67d6cc6ca01da3ae32de02d5',1,'util.h']]]
+  ['rdesc_5ftk_5fprinter_5ffunc_0',['rdesc_tk_printer_func',['../util_8h.html#a5ad7949e67d6cc6ca01da3ae32de02d5',1,'util.h']]],
+  ['rdesc_5ftoken_5fdestroyer_5ffunc_1',['rdesc_token_destroyer_func',['../rdesc_8h.html#a5fa070c0c999e4c999de685375c29230',1,'rdesc.h']]]
 ];
